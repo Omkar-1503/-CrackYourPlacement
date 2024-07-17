52748b241daf7bf44ad2dc4b5a31af54d145ab7f
“Omkar <omkarkhadakkar13@gmail.com>
2024-07-17 11:14:21 +0530

    Day 2

---
+++ Graph/BFS of graph.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<bool> vis(V, false);
+        int s = 0;
+
+        
+        vis[s] = true;
+
+        vector<int> res;
+
+        
+        queue<int> q;
+        q.push(s);
+
+        while (!q.empty()) {
+            int t = q.front();
+            res.push_back(t);
+            q.pop();
+
+            for (int v : adj[t]) {
+                if (!vis[v]) {
+                    vis[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+        return res;
+    }
+};
